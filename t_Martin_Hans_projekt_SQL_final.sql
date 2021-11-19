95d15d31155f9eff376244534a0d8f06cdedb375
MartinHans1 <92949036+MartinHans1@users.noreply.github.com>
2021-11-19 19:35:57 +0100

    Final script almost done

--- t_Martin_Hans_projekt_SQL_final.sql
+++ t_Martin_Hans_projekt_SQL_final.sql
@@ -1,4 +1,4 @@
---- parciální splnìní úkolù ---
+--- parciální øešení úkolù½ ---
 
 select 
   date,
@@ -27,15 +27,35 @@ select
    tests_performed
 from covid19_tests ct;
 
-
-
----- final script
+select
+  w.city,
+  w.date,
+  cast(w2.wind as integer) as wind_number 
+from weather w
+join (select 
+  date,
+  city,
+  substring (wind,1,2) as wind
+from weather w) w2
+on w.city=w2.city 
+and w.date=w2.date
+where w.city is not null 
+group by w.city,w.date;
 
 select 
-   country,
-   population_density,
-   median_age_2018
-from countries c;
+  date, 
+  city, 
+  replace (temp,'°c',' ') as temp
+from weather
+where city is not null;
+
+select 
+ date,
+ city,
+ substring (wind,1,2) as wind
+from weather w;
+
+---- final script
 
 create table covid19_info as
 select 
@@ -71,17 +91,6 @@ select country, GDP, population
 from economies e2
 where year=2020 group by country) a on a.country=e.country
 group by e.country;
-
-
-select ci.country,ci.date, ci.median_age_2018, ce.gdp_per_capita
-from covid19_info ci
-join covid19_economies ce 
-on ci.country=ce.country
-join covid19_religions_pivot_final crpf 
-on crpf.country=ci.country
-join covid19_life_expectancy cle
-on ci.country=cle.country
- ;
 
 create table covid19_religion_rel_share
 SELECT r.country , r.religion , 
@@ -152,24 +161,134 @@ FROM (
     ) b
     ON a.country = b.country
 ;
-
-create table covid19_temperature as
+     
 select 
-	w.date,
-	c.country,
-	w.city,
-	w2.avg_temp
-from weather w 
+ci.country,
+ci.date, 
+ci.median_age_2018, 
+ce.gdp_per_capita,
+ce.gini_coeficient,
+ce.child_mortality,
+cle.life_exp_diff,
+crpf.Buddhism,
+crpf.Christianity,
+crpf.Folk_Religions,
+crpf.Hinduism,
+crpf.Islam,
+crpf.Judaism,
+crpf.Other_Religions,
+crpf.Unaffiliated_Religions
+from covid19_info ci
+join covid19_economies ce 
+on ci.country=ce.country
+join covid19_religions_pivot_final crpf 
+on crpf.country=ci.country
+join covid19_life_expectancy cle
+on ci.country=cle.country
+join covid19_temp_final ctf
+on ci.country=ctf.country and 
+ci.date=ctf.date
+join covid19_hours_rain_final chrf 
+on ci.country=chrf.country and 
+ci.date=chrf.date
+join covid19_wind_final cwf 
+on ci.country=cwf.country and 
+ci.date=cwf.date;
+
+       
+-- pï¿½evodnï¿½kovï¿½ selecty texty na ï¿½ï¿½sla --
+
+create table covid19_temper_wind_convert as
+select
+  w.city,
+  w.date,
+  w.time,
+  cast(w2.temp as decimal) as temp, 
+  cast(w2.wind as decimal) as wind
+from weather w
+join (select 
+  date, 
+  city,
+  time,
+  replace (temp,'°c',' ') as temp,
+  substring (wind,1,2) as wind
+from weather) w2
+on w.city=w2.city 
+and w.date=w2.date
+and w.time=w2.time
+where w.city is not null 
+group by w.city,w.date, w.time;
+
+create table covid19_countries
+select 
+    lt.country,
+    c.capital_city
+from lookup_table lt
 join countries c 
-on w.city=c.capital_city
-join (select `date`, city, round (avg(temp),2) as avg_temp
-	  from weather w
-	  where time between '09:00' and '18:00'
-	  group by date, city) w2
-	  on w2.city=w.city 
-	  and w2.date=w.`date` 
-group by w.date	  
-order by w.`date`;
+on lt.iso3=c.iso3;
+
+create table covid19_temp_final as
+select
+	ctwc.date,
+	c.country,
+	round (avg (ctwc.temp),2) as avg_temp
+from covid19_temper_wind_convert ctwc 
+join countries c
+on c.capital_city=ctwc.city
+where city is not null and ctwc.time between '09:00' and '18:00'
+group by ctwc.date, c.country;
+
+create table covid19_wind_final as
+select
+	ctwc.date,
+	c.country,
+	max (ctwc.wind) as max_wind
+from covid19_temper_wind_convert ctwc
+join countries c 
+on c.capital_city=ctwc.city
+where ctwc.city is not null 
+group by ctwc.date, c.country;
+
+CREATE TABLE covid19_hours_rain_conversion AS
+select 
+	`date`, 
+	city, 
+	case when rain != '0.0 mm' then 3 else 0 end as hours_rain 
+from weather w
+where city is not null
+group by date,city, time;
+
+create table covid19_hours_rain_final
+select 
+    chrc.date, 
+    c.country,
+    sum (chrc.hours_rain) as hours_rain
+from countries c 
+join covid19_hours_rain_conversion chrc 
+on c.capital_city=chrc.city 
+group by chrc.date, c.country;
+
+from covid19_temper_wind_convert ctwc
+join (select 
+	date,
+	city,
+	temp
+	from covid19_temper_wind_convert ctwc2
+	where time between '09:00' and '18:00') ctwc2
+on ctwc.city=ctwc2.city
+join countries c 
+on c.capital_city=ctwc.city
+where ctwc.city is not null 
+group by ctwc.date, c.country;
+
+
+select 
+  date, 
+  city, 
+  replace (wind,'km/h from%',' ') as wind
+from weather;
+
+--- nefunkèní pokusy s weather ---
 
 create view covid19_weather as
 select 
@@ -202,23 +321,23 @@ join (select `date`, city, round (max(wind),2) as max_wind
 group by w.date, c.country	  
 order by w.`date`;
 
-create table covid19_rain
+create table covid19_temperature as
 select 
-    w.date,
-    c.country,
-    w.city,
-    sum (w2.hours_rain)
-    from weather w 
+	w.date,
+	c.country,
+	w.city,
+	w2.avg_temp
+from weather w 
 join countries c 
- on w.city=c.capital_city 
-join (select `date`, city, case when rain != '0.0 mm' then 3 else 0 end as hours_rain 
-       from weather w
-       where city is not null
-       group by date,city,time) w2
-       on w2.city=c.capital_city
-       and w2.date=w.`date` 
- group by w2.`date` 
- order by w2.`date`; 
+on w.city=c.capital_city
+join (select `date`, city, round (avg(temp),2) as avg_temp
+	  from weather w
+	  where time between '09:00' and '18:00'
+	  group by date, city) w2
+	  on w2.city=w.city 
+	  and w2.date=w.`date` 
+group by w.date	  
+order by w.`date`;
 
 create view covid19_wind as
 select 
@@ -237,55 +356,25 @@ join (select `date`, city, round (max(wind),2) as max_wind
 group by w.date	  
 order by w.`date`;
 
+create table covid19_temp__wind_final as
+select
+	ctwc.date,
+	c.country,
+	max (ctwc.wind) as max_wind,
+	round (avg (ctwc2.temp),2) as avg_temp
+from covid19_temper_wind_convert ctwc
+join (select 
+	date,
+	city,
+	temp
+	from covid19_temper_wind_convert ctwc2
+	where time between '09:00' and '18:00') ctwc2
+on ctwc.city=ctwc2.city
+join countries c 
+on c.capital_city=ctwc.city
+where ctwc.city is not null 
+group by ctwc.date, c.country;
 
-CREATE TABLE t_hours_rain AS
-select `date`, city, case when rain != '0.0 mm' then 3 else 0 end as hours_rain 
-       from weather w
-       where city is not null
-       group by date,city,time;
-
-select 
-    thr.date, 
-    c.country,
-    sum (thr.hours_rain)
-from countries c 
-join t_hours_rain thr 
-on c.capital_city=thr.city 
-group by thr.date, c.country;
-     
-select `date`, city, sum (hours_rain) 
-       from t_hours_rain thr 
-       where city is not null
-       group by date,city;
-            
-
-create table weather_converted as
-select 
-w.city,
-w.date,
-max(w2.wind) as max_wind,
-avg(w2.temp) as avg_temp
-from weather w
-join (select
-city,
-date,
-cast(wind as integer) as wind,
-cast(temp as integer) as temp 
-from weather w2) w2
-on w.date=w2.date
-and w2.city=w.city
-where w.city is not null
-group by city,date;
-
-select wind
-from weather w 
-order by convert (substring(wind, 7), signed integer);
-
-select cast(wind as integer) from weather w;
-
--- pøevodníkové selecty texty na èísla --
-
-create table covid19_temperature_convert as
 select
   w.city,
   w.date,
@@ -302,38 +391,43 @@ and w.date=w2.date
 where w.city is not null 
 group by w.city,w.date;
 
-select
-  w.city,
-  w.date,
-  cast(w2.wind as integer) as wind_number 
+create table covid19_rain
+select 
+    w.date,
+    c.country,
+    w.city,
+    sum (w2.hours_rain)
+    from weather w 
+join countries c 
+ on w.city=c.capital_city 
+join (select `date`, city, case when rain != '0.0 mm' then 3 else 0 end as hours_rain 
+       from weather w
+       where city is not null
+       group by date,city,time) w2
+       on w2.city=c.capital_city
+       and w2.date=w.`date` 
+ group by w2.`date` 
+ order by w2.`date`; 
+ 
+create table weather_converted as
+select 
+	w.city,
+	w.date,
+	max(w2.wind) as max_wind,
+	avg(w2.temp) as avg_temp
 from weather w
-join (select 
-  date,
-  city,
-  substring (wind,1,2) as wind
-from weather w) w2
-on w.city=w2.city 
-and w.date=w2.date
-where w.city is not null 
-group by w.city,w.date;
+join (select
+	city,
+	date,
+	cast(wind as integer) as wind,
+	cast(temp as integer) as temp 
+from weather w2) w2
+on w.date=w2.date
+and w2.city=w.city
+where w.city is not null
+group by city,date;
 
-
-select 
-  date, 
-  city, 
-  replace (temp,'°c',' ') as temp
-from weather
-where city is not null;
-
-select 
- date,
- city,
- substring (wind,1,2) as wind
-from weather w;
-
-select 
-  date, 
-  city, 
-  replace (wind,'km/h from%',' ') as wind
-from weather;
-
+select `date`, city, sum (hours_rain) 
+       from t_hours_rain thr 
+       where city is not null
+       group by date,city;
